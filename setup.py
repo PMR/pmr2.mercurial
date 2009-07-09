499358fb7e50f70da5851f837fb19b53ebd08303
tyu030 <tyu030@eb92df5b-a40c-0410-b02c-bf52feaaa166>
2009-07-09 06:59:32 +0000

    Moving to Mercurial 1.3.
    git-svn-id: https://svn.physiomeproject.org/svn/physiome/pmr2.mercurial/trunk@3677 eb92df5b-a40c-0410-b02c-bf52feaaa166

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(name='pmr2.mercurial',
       install_requires=[
           'setuptools',
           # -*- Extra requirements: -*-
-          'mercurial==1.0.2',
+          'mercurial==1.3',
       ],
       entry_points="""
       # -*- Entry points: -*-
