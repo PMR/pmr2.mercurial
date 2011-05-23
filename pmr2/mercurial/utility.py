2616b7a7121a5a82385dd8580fa7e00afd467338
tyu030 <tyu030@eb92df5b-a40c-0410-b02c-bf52feaaa166>
2011-05-23 05:07:51 +0000

    Typo.
    git-svn-id: https://svn.physiomeproject.org/svn/physiome/pmr2.mercurial/trunk@5863 eb92df5b-a40c-0410-b02c-bf52feaaa166

--- pmr2/mercurial/utility.py
+++ pmr2/mercurial/utility.py
@@ -218,7 +218,7 @@ class MercurialStorage(BaseStorage):
                     p = '%s/file/%s' % (url, rev)
                 result = self.format(**{
                     'permissions': 'lrwxrwxrwx',
-                    'type': 'external',
+                    'contenttype': 'external',
                     'node': self.rev,
                     'date': '',
                     'size': '',
