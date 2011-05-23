6522b29547d92836491fc66dbe47a7ffeb2805c1
tyu030 <tyu030@eb92df5b-a40c-0410-b02c-bf52feaaa166>
2011-05-23 05:07:52 +0000

    Enabling icons for workspace file browser.
    Progress towards:
    https://tracker.physiomeproject.org/show_bug.cgi?id=2615
    
    git-svn-id: https://svn.physiomeproject.org/svn/physiome/pmr2.mercurial/trunk@5864 eb92df5b-a40c-0410-b02c-bf52feaaa166

--- pmr2/mercurial/interfaces.py
+++ pmr2/mercurial/interfaces.py
@@ -2,6 +2,12 @@ import zope.schema
 import zope.interface
 
 
+class IAppLayer(zope.interface.Interface):
+    """\
+    Marker interface for this product.
+    """
+
+
 class IPMR2HgWorkspaceAdapter(zope.interface.Interface):
     """\
     Adapter class between PMR2 content class and pmr2.mercurial
--- pmr2/mercurial/utility.py
+++ pmr2/mercurial/utility.py
@@ -218,7 +218,7 @@ class MercurialStorage(BaseStorage):
                     p = '%s/file/%s' % (url, rev)
                 result = self.format(**{
                     'permissions': 'lrwxrwxrwx',
-                    'contenttype': 'external',
+                    'contenttype': repotype,
                     'node': self.rev,
                     'date': '',
                     'size': '',
