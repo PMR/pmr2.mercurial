27631b69e0affbaefcae9b751d1527b887015e39
Tommy Yu <tommy.yu@auckland.ac.nz>
2012-03-23 18:41:27 +1300

    Avoid running the tests that need ZServer.

--- pmr2/mercurial/tests/test_doctest.py
+++ pmr2/mercurial/tests/test_doctest.py
@@ -1,4 +1,4 @@
-import unittest
+from unittest import TestCase, TestSuite, makeSuite
 import doctest
 
 from Testing import ZopeTestCase as ztc
@@ -6,15 +6,18 @@ from Testing import ZopeTestCase as ztc
 from pmr2.mercurial.tests import base
 
 def test_suite():
-    return unittest.TestSuite([
-
-        ztc.ZopeDocFileSuite(
-            'browser.txt', package='pmr2.mercurial',
-            test_class=base.MercurialDocTestCase,
-            optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
-        ),
-
-    ])
+    browser = ztc.ZopeDocFileSuite(
+        'browser.txt', package='pmr2.mercurial',
+        test_class=base.MercurialDocTestCase,
+        optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
+    )
+    synchronize = ztc.ZopeDocFileSuite(
+        'synchronize.txt', package='pmr2.mercurial',
+        test_class=base.MercurialDocTestCase,
+        optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
+    )
+    synchronize.level = 9
+    return TestSuite([browser, synchronize])
 
 if __name__ == '__main__':
     unittest.main(defaultTest='test_suite')
