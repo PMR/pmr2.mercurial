9fc6cc457e099c24c788ef6df824936c47374509
Tommy Yu <tommy.yu@auckland.ac.nz>
2013-08-23 17:49:06 +1200

    Bumping to the latest version that actually works.
    - Eventually will need to look into hglib instead.  CBF trying to chase
      after "APIs" that will be broken after a while.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(name='pmr2.mercurial',
       install_requires=[
           'setuptools',
           # -*- Extra requirements: -*-
-          'mercurial==2.2.1',
+          'mercurial==2.2.3',
       ],
       entry_points="""
       # -*- Entry points: -*-
