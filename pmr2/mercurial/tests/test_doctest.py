b5d0e1cf6b471e1cf9eee15500f247c7e4ca0766
Tommy Yu <tommy.yu@auckland.ac.nz>
2014-06-10 18:35:17 +1200

    Migrate the synchorize test to use layers
    - Still need to keep the ztc.FunctionalTestCase as the base class due to
      the need to "mimic" http access to the local site to avoid the server
      from being blocked from accessing itself during the tests.
    - Drop the test level since this should no longer cause problems, even
      though it seems like the server thread can be properly killed now.

--- pmr2/mercurial/tests/test_doctest.py
+++ pmr2/mercurial/tests/test_doctest.py
@@ -1,9 +1,12 @@
 from unittest import TestCase, TestSuite, makeSuite
 import doctest
 
+from plone.testing import layered
 from Testing import ZopeTestCase as ztc
 
 from pmr2.mercurial.tests import base
+from pmr2.mercurial.tests import layer
+
 
 def test_suite():
     browser = ztc.ZopeDocFileSuite(
@@ -11,12 +14,13 @@ def test_suite():
         test_class=base.MercurialDocTestCase,
         optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
     )
-    synchronize = ztc.ZopeDocFileSuite(
-        'synchronize.txt', package='pmr2.mercurial',
-        test_class=base.MercurialDocTestCase,
-        optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
+    synchronize = layered(ztc.ZopeDocFileSuite(
+            'synchronize.txt', package='pmr2.mercurial',
+            test_class=ztc.FunctionalTestCase,
+            optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
+        ),
+        layer=layer.MERCURIAL_LIVE_FUNCTIONAL_LAYER
     )
-    synchronize.level = 9001
     return TestSuite([browser, synchronize])
 
 if __name__ == '__main__':
